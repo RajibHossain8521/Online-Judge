44b00fe2b83ad011ce81e5acfe8637891a764fca
rajibhossain8521 <rajibhossain8521@gmail.com>
2020-10-08 17:25:50 +0600

    Big Mod, UVA-374 Problem Solved

---
+++ UVA374.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int mod(int B, int P, int M)
+{
+    if (P==0)
+        return 1;
+    if (P%2==0)
+    {
+        int x = mod(B, P/2, M);
+        return (x*x)%M;
+    }
+    else
+        return (B%M * mod(B, P-1, M))%M;
+}
+
+int main()
+{
+    int base, power, mod_value;
+    while(cin>>base>>power>>mod_value)
+    {
+        cout << mod(base, power, mod_value) << endl;
+    }
+    return 0;
+}
